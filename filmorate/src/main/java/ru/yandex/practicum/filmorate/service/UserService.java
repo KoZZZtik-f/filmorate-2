5d9f1a54b0a74b57e8396647254f99f9584081e8
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-01-31 13:37:01 +0300

    feat: созданы классы FilmService и UserService

---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -0,0 +1,42 @@
+package ru.yandex.practicum.filmorate.service;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserStorage userStorage;
+
+
+    public User createUser(User user) {
+
+        return userStorage.createUser(user);
+    }
+
+    public User updateUser(User user) {
+        return userStorage.updateUser(user);
+    }
+
+    public User getUserById(int id) {
+        return userStorage.getUserById(id);
+    }
+
+    public Collection<User> getAllUsers() {
+        return userStorage.getAllUsers();
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("✅ UserService создан, userStorage = " + (userStorage != null ? "OK" : "NULL"));
+        System.out.println(this != null);
+    }
+
+}
