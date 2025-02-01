f4e0febc2e6fa46d348ab010e422bc4c60789ac0
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-02-01 13:01:37 +0300

    feat: добавлен класс @RestControllerAdvice ErrorHandler для обработки ошибок + класс ErrorResponse для удобного представления ошибок

---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptionhandler/ErrorHandler.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.filmorate.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.controller.UserController;
+
+import java.util.Map;
+
+@RestControllerAdvice(assignableTypes = {FilmController.class, UserController.class})
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleException(final RuntimeException e) {
+        return new ErrorResponse("error", "description");
+    }
+
+
+
+}
---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptionhandler/ErrorResponse.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.exceptionhandler;
+
+import lombok.Data;
+
+@Data
+public class ErrorResponse {
+
+    private String error;
+
+    private String description;
+
+    public ErrorResponse(String error, String description) {
+        this.error = error;
+        this.description = description;
+    }
+}
