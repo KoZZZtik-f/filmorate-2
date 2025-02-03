3c366459a77700194ee0d10cfd1c309afda0d5bb
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-02-03 08:10:27 +0300

    chore: добавлена обработка NotFoundException в классе ErrorHandler

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptionhandler/ErrorHandler.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptionhandler/ErrorHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
 @RestControllerAdvice(assignableTypes = {FilmController.class, UserController.class})
 public class ErrorHandler {
@@ -14,6 +15,12 @@ public class ErrorHandler {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ErrorResponse handleException(final RuntimeException e) {
         return new ErrorResponse("error", "description");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(final NotFoundException e) {
+        return new ErrorResponse(NotFoundException.class.getSimpleName(), e.getMessage());
     }
 
 
