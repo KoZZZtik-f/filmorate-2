19860558f9f9901851ea20e69ac36c2f8e6de9b8
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-02-03 08:16:28 +0300

    chore: в Service классах реализована проверка NotFound перед полуением чего-либо

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.util.Collection;
 
@@ -15,7 +17,10 @@ import java.util.Collection;
 @RequiredArgsConstructor
 public class FilmService {
 
+    private static final String NOT_FOUND_MESSAGE = "Film с id %id не найден";
     private final FilmStorage filmStorage;
+    private final UserStorage userStorage;
+    private final UserService userService;
 
 
     public Film createFilm(Film film) {
@@ -23,10 +28,12 @@ public class FilmService {
     }
 
     public Film updateFilm(Film film) {
+        checkFilmNotFound(film);
         return filmStorage.updateFilm(film);
     }
 
     public Film getFilmById(int id) {
+        checkFilmNotFound(id);
         return filmStorage.getFilmById(id);
     }
 
@@ -35,20 +42,40 @@ public class FilmService {
     }
 
     public void addLike(Integer id, Integer userId) {
-        log.info("Film debug {}", getFilmById(id) );
+        checkFilmNotFound(userId);
+        userService.checkUserNotFound(id);
+
         Film film = filmStorage.getFilmById(id);
         film.getLikes().add(userId);
-        log.info("User {} liked fiml {} ({}). Likes = {}", userId, film.getId(), film.getName(), film.getLikes().size());
+        log.info("User {} liked fiml {} ({}). Likes = {}", userId , film.getId(), film.getName(), film.getLikes().size());
     }
 
-    public void removeLike(Integer id, Integer userId) {
+    public void removeLike(Integer id, Integer filmId) {
+        checkFilmNotFound(filmId);
+        userService.checkUserNotFound(id);
+
         Film film = filmStorage.getFilmById(id);
-        film.getLikes().remove(userId);
-        log.info("User {} remove like fiml {} ({}). Likes = {}", userId, film.getId(), film.getName(), film.getLikes().size());
+        film.getLikes().remove(filmId);
+        log.info("User {} remove like fiml {} ({}). Likes = {}", filmId, film.getId(), film.getName(), film.getLikes().size());
     }
 
     public Collection<Film> getMostPopularFilms(Integer count) {
         return filmStorage.getMostPopularFilms(count);
     }
+
+
+    public void checkFilmNotFound(Integer...  ids) {
+        for (Integer id : ids) {
+            if (!filmStorage.contains(id)) {
+                throw new NotFoundException(String.format(NOT_FOUND_MESSAGE, id));
+            }
+        }
+    }
+    public void checkFilmNotFound(Film... films) {
+        for (Film film : films) {
+            checkFilmNotFound(film.getId());
+        }
+    }
+
 
 }
