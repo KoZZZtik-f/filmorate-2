559eaed94708e0a29a321b394e428eb82b7e28ee
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-02-05 09:40:12 +0300

    feat: создан интерфейс и класс для хранения лайков LikeStorage

---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDbStorage.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.filmorate.storage.like;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LikeDbStorage implements LikeStorage {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void addLike(int filmId, int userId) {
+        final String sql = "insert into likes (film_id, user_id) values (?, ?)";
+
+        jdbcTemplate.update(sql, filmId, userId);
+    }
+
+    @Override
+    public void removeLike(int filmId, int userId) {
+        final String sql = "delete from likes where film_id = ? and user_id = ?";
+
+        jdbcTemplate.update(sql, filmId, userId);
+    }
+}
