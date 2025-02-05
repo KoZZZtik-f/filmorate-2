33382fe2084ae38b1ba67745e59ec5743d369a94
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-02-05 09:35:42 +0300

    feat: в ErrorHandler добавлена обработка AlreadyFriendsException

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptionhandler/ErrorHandler.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/exceptionhandler/ErrorHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
+import ru.yandex.practicum.filmorate.exception.AlreadyFriendsException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
 @RestControllerAdvice(assignableTypes = {FilmController.class, UserController.class})
@@ -14,13 +15,19 @@ public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ErrorResponse handleException(final RuntimeException e) {
-        return new ErrorResponse("error", "description");
+        return new ErrorResponse(e.getClass().getSimpleName(), e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFound(final NotFoundException e) {
         return new ErrorResponse(NotFoundException.class.getSimpleName(), e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    public ErrorResponse handleAlreadyFriendsException(final AlreadyFriendsException e) {
+        return new ErrorResponse(AlreadyFriendsException.class.getSimpleName(), e.getMessage());
     }
 
 
