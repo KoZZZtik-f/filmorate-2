698084fce8bed70fcc52002be42190ea0ff0394e
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-10 12:31:54 +0300

    refactor: в AlreadyFriendsException добавлено сообщение по умолчанию, но можно писать свое

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/exception/AlreadyFriendsException.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/exception/AlreadyFriendsException.java
@@ -1,6 +1,13 @@
 package ru.yandex.practicum.filmorate.exception;
 
 public class AlreadyFriendsException extends RuntimeException {
+
+    private static final String DEFAULT_MESSAGE = "Пользователи уже являются друзьями";
+
+    public AlreadyFriendsException() {
+        super(DEFAULT_MESSAGE);
+    }
+
     public AlreadyFriendsException(String message) {
         super(message);
     }
