d05889252da35c12e89f9c4a7070d230f3153146
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-10 15:03:24 +0300

    feat: добавлено полное логирование в FilmController

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -19,34 +19,37 @@ public class FilmController {
 
     @PostMapping
     public Film createFilm(@Valid @RequestBody Film film) {
-        log.info("Create film {}, id = {}", film.getName(), film.getId());
+        log.info("Received request to create film with name: {}", film.getName());
         return filmService.createFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
-        log.info("Update film {}", film.getName());
+        log.info("Received request to update film with name: {}", film.getName());
         return filmService.updateFilm(film);
     }
 
     @GetMapping
     public Collection<Film> getAllFilms() {
+        log.info("Received request to get all films");
         return filmService.getAllFilms();
     }
 
     @GetMapping("/{id}")
     public Film getFilmById(@PathVariable("id") Integer id) {
+        log.info("Received request to get film by ID: {}", id);
         return filmService.getFilmById(id);
     }
     
     @PutMapping("/{id}/like/{userId}")
     public void addLike(@PathVariable("id") Integer id, @PathVariable("userId") Integer userId) {
-        log.info("Received request to add like for film {} by user {}", id, userId);
+        log.info("Received request to add like for film with ID: {} by user with ID: {}", id, userId);
         filmService.addLike(id, userId);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
     public void removeLike(@PathVariable("id") Integer id, @PathVariable("userId") Integer userId) {
+        log.info("Received request to remove like for film with ID: {} by user with ID: {}", id, userId);
         filmService.removeLike(id, userId);
     }
 
@@ -56,6 +59,7 @@ public class FilmController {
         if (count < 0) {
             throw new RuntimeException();
         }
+        log.info("Received request to get popular films with count: {}", count);
         return filmService.getMostPopularFilms(count);
     }
 
