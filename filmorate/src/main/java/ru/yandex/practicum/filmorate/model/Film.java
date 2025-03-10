6aa4bc9a5185d331fcdf97dd9aa070ae6250e228
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-10 17:19:31 +0300

    feat: в Film и User классы добавлена валидация @NotNull на нужные поля

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,11 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.time.DurationMin;
 import ru.yandex.practicum.filmorate.serializer.DurationSerializer;
+import ru.yandex.practicum.filmorate.validator.ReleaseDateValidation;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -15,24 +16,30 @@ import java.util.Set;
 @Data
 public class Film {
 
+    @NotNull(message = "ID cannot be null")
     private int id;
 
+    @NotNull(message = "Name cannot be null")
     @NotBlank(message = "Name cannot be blank")
     private String name;
 
-    @Size(max = 200, message = "Description must be less, than 200 characters")
+    @NotNull(message = "Description cannot be null")
+    @Size(max = 200, message = "Description must be less than 200 characters")
     private String description;
 
+    @NotNull(message = "Release date cannot be null")
+    @ReleaseDateValidation(message = "Release date must be on or after December 28, 1895")
     private LocalDate releaseDate;
 
-    @DurationMin(seconds = 1)
+    @NotNull(message = "Duration cannot be null")
+    @DurationMin(seconds = 1, message = "Duration must be at least 1 second")
     @JsonSerialize(using = DurationSerializer.class)
     private Duration duration;
 
-    @JsonIgnore
     private Set<Integer> likes;
 
     public Film() {
         likes = new HashSet<>();
     }
+
 }
--- filmorate/src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import lombok.Data;
 
@@ -13,25 +14,26 @@ import java.util.Set;
 @Data
 public class User {
 
-    public User() {
-        friends = new HashSet<>();
-    }
-
     private Integer id;
 
+    @NotNull(message = "Email cannot be null")
     @Email(message = "Invalid email address")
     private String email;
 
+    @NotNull(message = "Login cannot be null")
     @NotBlank(message = "Login cannot be blank")
     private String login;
 
-    //If empty -> name = login (In Service)
+    // If name is null, then name = login (in service)
     private String name;
 
-    @PastOrPresent(message = "Birthday must be in past")
+    @NotNull(message = "Birthday cannot be null")
+    @PastOrPresent(message = "Birthday must be in the past or present")
     private LocalDate birthday;
 
-    @JsonIgnore
-    Set<Integer> friends;
+    private Set<Integer> friends;
 
+    public User() {
+        friends = new HashSet<>();
+    }
 }
