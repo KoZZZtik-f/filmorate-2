2ba7800ca4b60e6717b60dbfc6b8b6ab211d9e80
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-10 17:23:45 +0300

    fix: метод removeLike теперь выбрасывает исключение, если в него передается несуществующий film_id или user_id

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDbStorage.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDbStorage.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.storage.like;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
 
 @Component
 @RequiredArgsConstructor
@@ -21,6 +22,9 @@ public class LikeDbStorage implements LikeStorage {
     public void removeLike(int filmId, int userId) {
         final String sql = "delete from likes where film_id = ? and user_id = ?";
 
-        jdbcTemplate.update(sql, filmId, userId);
+        Integer updatedRowsCount = jdbcTemplate.update(sql, filmId, userId);
+        if (updatedRowsCount == 0) {
+            throw new NotFoundException(String.format("Either film (%d) or user (%d) not exist.", filmId, userId) );
+        }
     }
 }
