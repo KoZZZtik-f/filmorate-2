00c4e7a607a54470643fa123f8ca820f938c3c48
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-11 21:27:20 +0300

    chore: поправлены импорты в Film классе

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.time.DurationMin;
 import ru.yandex.practicum.filmorate.serializer.DurationSerializer;
 import ru.yandex.practicum.filmorate.validator.ReleaseDateValidation;
