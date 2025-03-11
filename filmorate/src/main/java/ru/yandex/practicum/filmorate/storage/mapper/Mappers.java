c12361e772b93c071a13ab79d59851070d827141
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-11 21:23:44 +0300

    feat: в Mappers классе создан genreRowMapper

--- filmorate/src/main/java/ru/yandex/practicum/filmorate/storage/mapper/Mappers.java
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/storage/mapper/Mappers.java
@@ -3,8 +3,11 @@ package ru.yandex.practicum.filmorate.storage.mapper;
 import lombok.Getter;
 import org.springframework.jdbc.core.RowMapper;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.Duration;
 
 public class Mappers {
@@ -28,8 +31,16 @@ public class Mappers {
         film.setDescription(resultSet.getString("description"));
         film.setReleaseDate(resultSet.getDate("release_date").toLocalDate());
         film.setDuration(Duration.ofSeconds(resultSet.getInt("duration")));
+        film.setGenreId(resultSet.getInt("genre_id"));
         return film;
     };
 
+    @Getter
+    private static final RowMapper<Genre> genreRowMapper = (resultSet, rowNum) -> {
+        Genre genre = new Genre();
+        genre.setId(resultSet.getInt("id"));
+        genre.setName(resultSet.getString("name"));
+        return genre;
+    };
 
 }
