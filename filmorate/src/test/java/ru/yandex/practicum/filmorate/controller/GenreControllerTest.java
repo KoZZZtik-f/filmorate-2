6cd2650460de74c2310f85efd5a86a0a5bff23c3
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-03-14 21:30:01 +0300

    test: создан класс GenreControllerTest

---
+++ filmorate/src/test/java/ru/yandex/practicum/filmorate/controller/GenreControllerTest.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class GenreControllerTest {
+}
